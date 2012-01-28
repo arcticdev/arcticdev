d727861912ce725cc83bdc71d23d5e33e0d6f20a
schnek <tolstikov.misha@gmail.com>
2012-01-28 22:55:21 +0400

    * fixed PrecompiledHeader...

--- make_precompiled_header_debug.sh
+++ make_precompiled_header_debug.sh
@@ -2,6 +2,7 @@
 
 echo Building Precompiled Header...
 cd src/WorldServer
-g++ -pthread -Wall -g -DHAVE_CONFIG_H -I. -I../.. -I/usr/include/mysql -I/usr/include/openssl -I/usr/local/include/openssl -I/usr/local/include -I. -I./../libs -I./../libs/vmap -I./../libs/g3dlite -o StdAfx.h.gch StdAfx.h
++g++ -std=c++0x -pthread -Wall -g -DHAVE_CONFIG_H -I. -I../.. -I/usr/include/mysql -I/usr/include/openssl -I/usr/local/include/openssl -I/usr/local/include -I. -I./../libs -I./../libs/Collision/vmap -I./../libs/Collision/g3dlite -o StdAfx.h.gch StdAfx.h
 
+echo All has passed well!
 echo Done. Now you can run make, and experience much faster build times. Remember to re-run this script every time you svn update.
